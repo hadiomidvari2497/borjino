161ea33f3d672b9542453c52e94b4835e4ee5082
hadiomidvari2497 <hadiomidvari2497@gmail.com>
2026-09-15 19:19:57 +0330

    chore: remove legacy application architecture

--- database/migrations/014_enforce_unit_block_building.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE blocks
-    ADD UNIQUE KEY uq_blocks_id_building (id, building_id);
-
-ALTER TABLE units
-    DROP FOREIGN KEY fk_units_block,
-    ADD CONSTRAINT fk_units_block_building
-        FOREIGN KEY (block_id, building_id) REFERENCES blocks(id, building_id)
-        ON UPDATE CASCADE ON DELETE RESTRICT;
--- resources/views/home/index.php
+++
@@ -1,4 +0,0 @@
-<main dir="rtl">
-    <h1><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></h1>
-    <p>هسته اولیه سامانه مدیریت ساختمان برجینو آماده است.</p>
-</main>
